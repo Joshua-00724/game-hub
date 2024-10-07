af8d65299a2410800c9baa801bb915758d904859
Joshua Ranbhise <ranbhisejoshua07@gmail.com>
2024-10-07 18:40:59 -0400

    Refactoring by moving entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,11 +1,10 @@
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emojis from "./Emojis";
 import { Link, useNavigate } from "react-router-dom";
-import { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   game: Game;
